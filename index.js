b8c1e47cb944ad3466989ea281747bfd73c5e325
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2025-12-10 20:24:39 +0000

    hjds

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let automobiles = ["car","truck","motercycle"];
+let automobilesCopy = ["automobiles"];
+automobiles.push("airplane","skateboard");
+automobiles.pop("skateboard");
+automobiles.unshift("bike","helicopter");
+automobiles.shift("bike");
 
+
+document.getElementById("result").innerHTML = automobiles;
+
+//should define a new variable called automobilesCopy and set it equal to a string template that contains the variable automobiles
